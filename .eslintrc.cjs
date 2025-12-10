d1c67887cc051ffa6fcca764a4f49e0ad6e06836
Ikenna Richard <oguejioforrichard@gmail.com>
2025-12-10 12:26:51 +0100

    chore: update ESLint rules and fix AppleProvider formatting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,9 @@ module.exports = {
   rules: {
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
+    quotes: "off",
+    "prettier/prettier": "off",
+    "@typescript-eslint/func-call-spacing": "off",
   },
   ignorePatterns: [
     "node_modules/",
